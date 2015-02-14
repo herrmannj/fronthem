a330c320fa2536998e483dd8a1512d382493b45e
Bernd Gewehr <bgewehr@users.noreply.github.com>
2015-02-14 11:39:48 +0100

    Update 99_fronthemUtils.pm
    added the UZSU execute and UZSU converter

--- FHEM/99_fronthemUtils.pm
+++ FHEM/99_fronthemUtils.pm
@@ -4,6 +4,7 @@ package main;
 
 use strict;
 use warnings;
+use JSON;
 
 sub
 fronthemUtils_Initialize($$)
@@ -11,9 +12,86 @@ fronthemUtils_Initialize($$)
   my ($hash) = @_;
 }
 
+
+###############################################################################
+#
+# Umsetzen der UZSU-Settings für ein device
+#
+###############################################################################
+sub UZSU_execute($$)
+{
+  my ($device, $uzsu) = @_;
+  
+  $uzsu = decode_json($uzsu);
+  fhem('delete wdt_'.$device.'_uzsu');
+  my $weekdays_part = " ";
+  for(my $i=0; $i < @{$uzsu->{list}}; $i++) {
+      my $weekdays = $uzsu->{list}[$i]->{rrule};
+      $weekdays = substr($weekdays,18,50);  
+      if (($uzsu->{list}[$i]->{active})) {
+          $weekdays_part = $weekdays_part.' '.$weekdays.'|'.$uzsu->{list}[$i]->{time}.'|'.$uzsu->{list}[$i]->{value};
+      }
+  }
+  fhem('define wdt_'.$device.'_uzsu'.' WeekdayTimer '.$device.' en '.$weekdays_part);
+  fhem('attr wdt_'.$device.'_uzsu room UZSU');
+  if ($uzsu->{active}){    
+     fhem('attr wdt_'.$device.'_uzsu disable 0');
+  } else {
+     fhem('attr wdt_'.$device.'_uzsu disable 1');
+  }    
+}
+
 package fronthem;
 use strict;
 use warnings;
+use JSON;
+
+###############################################################################
+# For use with UZSU-Widget in SV and UZSU-notify in fhem
+# Setreading a device reading using JSON conversion (gadval => reading=decode_json() => setval => encode_json(reading) )
+# the reading (e.g. "uzsu") must be created manually for each USU-enabled device in fhem using "setreading <device> uzsu {}"
+# in the fhem commandline
+###############################################################################
+
+sub UZSU(@)
+{
+  my ($param) = @_;
+  my $cmd = $param->{cmd};
+  my $gad = $param->{gad};
+  my $gadval = $param->{gadval};
+
+  my $device = $param->{device};
+  my $reading = $param->{reading};
+  my $event = $param->{event};
+  
+  my @args = @{$param->{args}};
+  my $cache = $param->{cache};
+
+  if ($param->{cmd} eq 'get')
+  {
+    $param->{cmd} = 'send';
+  }
+  if ($param->{cmd} eq 'send')
+  {
+    $param->{gad} = $gad;
+	$param->{gadval} = decode_json(main::ReadingsVal($device, $reading, ''));
+	$param->{gads} = [];
+    return undef;
+  }
+  elsif ($param->{cmd} eq 'rcv')
+  {
+	$gadval = encode_json($gadval);
+	$gadval =~ s/;/;;/ig;
+	$param->{result} = main::fhem("setreading $device $reading $gadval");
+	$param->{results} = [];
+    return 'done';
+  }
+  elsif ($param->{cmd} eq '?')
+  {
+    return 'usage: UZSU';
+  }
+  return undef;
+}
 
 ###############################################################################
 #
