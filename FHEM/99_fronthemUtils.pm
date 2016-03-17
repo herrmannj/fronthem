9f651c1cf536f8838d95c22369cd0375d3e58e97
Bernd Gewehr <bgewehr@users.noreply.github.com>
2016-03-17 21:37:19 +0100

    Update 99_fronthemUtils.pm
    adopted the uzsu_execute to the dev 4.3 JSON of MWORIONs UZSU_widget

--- FHEM/99_fronthemUtils.pm
+++ FHEM/99_fronthemUtils.pm
@@ -29,53 +29,57 @@ sub fronthem_encodejson($) {
 sub UZSU_execute($$)
 {
  my ($device, $uzsu) = @_;
+ my $weekdays = "";
 
  $uzsu = decode_json($uzsu);
 
- my $weekdays_part = " ";
+ fhem('delete wdt_uzsu_'.$device.'_.*');
+
  for(my $i=0; $i < @{$uzsu->{list}}; $i++) {
-     my $weekdays = $uzsu->{list}[$i]->{rrule};
+     $weekdays = $uzsu->{list}[$i]->{rrule};
      $weekdays = substr($weekdays,18,50);
+
      # if the structure contains the holidays list, use it!
-     if ({list}[$i]->{holidays}) {
-     	if ({list}[$i]->{holiday}->{weekend}) {
-	     	$weekdays = $weekdays . ',$we';
-     	}
-     	if ({list}[$i]->{holiday}->{work}) {
-	     	$weekdays = $weekdays . ',!$we';
-     	}
-     }
-     if (($uzsu->{list}[$i]->{active})) {
-         if ($uzsu->{list}[$i]->{event} eq 'time'){
-         $weekdays_part = $weekdays_part.' '.$weekdays.'|'.$uzsu->{list}[$i]->{time}.'|'.$uzsu->{list}[$i]->{value};
-         }
-       else {
-          # Bugfix below: because sunset_abs from 99_sunrise_el does not work if max-time = ""
-          if ($uzsu->{list}[$i]->{timeMin} ne '' and $uzsu->{list}[$i]->{timeMax} ne '') {
-            $weekdays_part = $weekdays_part.' '.$weekdays.'|{'.$uzsu->{list}[$i]->{event}.'_abs("REAL",'.$uzsu->{list}[$i]->{timeOffset} * 60 .',"'.$uzsu->{list}[$i]->{timeMin}.'","'.$uzsu->{list}[$i]->{timeMax}.'")}|'.$uzsu->{list}[$i]->{value};
-          } else {
-          $weekdays_part = $weekdays_part.' '.$weekdays.'|{'.$uzsu->{list}[$i]->{event}.'_abs("REAL",'.$uzsu->{list}[$i]->{timeOffset} * 60 .',,)}|'.$uzsu->{list}[$i]->{value};
-          }
-       }
-     }
-     # if the structure contains a condition, use it!
-     if ({list}[$i]->{condition}) {
-     	if ({list}[$i]->{condition}->{conditionType} eq 'Perl') {
-     		$weekdays_part = $weekdayspart.'|('.{list}[$i]->{condition}->{conditionDevicePerl}.')';
-     	} else {
-     		$weekdays_part = $weekdayspart.'|(ReadingsVal("'.{list}[$i]->{condition}->{conditionDevicePerl}.'","'.{list}[$i]->{condition}->{conditionType}.'","") eq "'.{list}[$i]->{condition}->{conditionValue}.'")';
-     	}
-     }
-	fhem('defmod wdt_uzsu_'.$device.'-'.$i.' WeekdayTimer '.$device.' en '.$weekdays_part);
- 	if ($uzsu->{active}){
-	 	fhem('attr wdt_uzsu_'.$device.' disable 0');
-	} else {
- 		fhem('attr wdt_uzsu_'.$device.' disable 1');
+     if ($uzsu->{list}[$i]->{holiday}->{weekend}) {
+	if ($weekdays ne '') {
+		$weekdays = $weekdays . ',';
 	}
- 	fhem('attr wdt_uzsu_'.$device.'-'.$i.' room UZSU');
- 	fhem('attr wdt_uzsu_'.$device.'-'.$i.' group '.$device);
- 	#fhem('save');   # use only if you want to save WDT settings immediately.
- } 
+     	$weekdays = $weekdays . '$we';
+     }
+     if ($uzsu->{list}[$i]->{holiday}->{work}) {
+	if ($weekdays ne '') {
+		$weekdays = $weekdays . ',';
+	}
+     	$weekdays = $weekdays . '!$we';
+     }
+     
+     if ($uzsu->{list}[$i]->{event} eq 'time'){
+         $weekdays = $weekdays.'|'.$uzsu->{list}[$i]->{time}.'|'.$uzsu->{list}[$i]->{value};
+     } else {
+         # Bugfix below: because sunset_abs from 99_sunrise_el does not work if max-time = ""
+         if ($uzsu->{list}[$i]->{timeMin} ne '' and $uzsu->{list}[$i]->{timeMax} ne '') {
+             $weekdays = $weekdays.'|{'.$uzsu->{list}[$i]->{event}.'_abs("REAL",'.$uzsu->{list}[$i]->{timeOffset} * 60 .',"'.$uzsu->{list}[$i]->{timeMin}.'","'.$uzsu->{list}[$i]->{timeMax}.'")}|'.$uzsu->{list}[$i]->{value};
+         } else {
+             $weekdays = $weekdays.'|{'.$uzsu->{list}[$i]->{event}.'_abs("REAL",'.$uzsu->{list}[$i]->{timeOffset} * 60 .',,)}|'.$uzsu->{list}[$i]->{value};
+         }
+     }
+     
+     # if the structure contains a condition, use it!
+     if ($uzsu->{list}[$i]->{condition}->{conditionActive}) {
+     	if ($uzsu->{list}[$i]->{condition}->{conditionType} eq 'Perl') {
+     		$weekdays = $weekdays.' ('.$uzsu->{list}[$i]->{condition}->{conditionDevicePerl}.')';
+     	} else {
+     		$weekdays = $weekdays.' (ReadingsVal("'.$uzsu->{list}[$i]->{condition}->{conditionDevicePerl}.'","state","") '.$uzsu->{list}[$i]->{condition}->{conditionType}.' "'.$uzsu->{list}[$i]->{condition}->{conditionValue}.'")';
+     	}
+     }
+
+     if ($uzsu->{list}[$i]->{active}){
+         fhem('defmod wdt_uzsu_'.$device.'_'.$i.' WeekdayTimer '.$device.' en '.$weekdays);
+         fhem('attr wdt_uzsu_'.$device.'_'.$i.' room UZSU');
+         fhem('attr wdt_uzsu_'.$device.'_'.$i.' group '.$device);
+     }
+     #fhem('save');   # use only if you want to save WDT settings immediately.
+ }
 }
 
 package fronthem;
